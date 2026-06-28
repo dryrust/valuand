b8d2aef2d25daa1615d088b8a142030ffd8808b3
Arto Bendiken <arto@bendiken.net>
2026-06-27 20:03:48 -0700

    Create the Dart package.

---
+++ dart/README.md
@@ -0,0 +1,1 @@
+# Valuand.dart: Valuand for Dart
---
+++ dart/test/valuand_test.dart
@@ -0,0 +1,6 @@
+// This is free and unencumbered software released into the public domain.
+
+import 'package:valuand/valuand.dart';
+import 'package:test/test.dart';
+
+void main() {}
